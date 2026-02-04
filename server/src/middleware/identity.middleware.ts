1311263637db7ffa53d74dfb8faaec876948a21a
adedamolabasit <adedamolabasit09@gmail.com>
2026-02-05 00:12:41 +0100

    chore: client header updates

--- server/src/middleware/identity.middleware.ts
+++ server/src/middleware/identity.middleware.ts
@@ -8,32 +8,24 @@ export async function identifyUser(
   next: NextFunction,
 ) {
   try {
-    const walletAddress = req.headers["wallet-address"] as string | undefined;
     const visitorId = req.headers["visitor-id"] as string | undefined;
 
     let userType: "user" | "visitor" | null = null;
     let identifier: string | undefined;
 
-    if (walletAddress !== "undefined") {
-      userType = "user";
-      identifier = walletAddress;
-    } else if (visitorId) {
+   if (visitorId) {
       userType = "visitor";
       identifier = visitorId;
     }
 
     if (!identifier || !userType) return next();
 
-    const query =
-      userType === "user"
-        ? { walletAddress: identifier }
-        : { visitorId: identifier };
+    const query =  { visitorId: identifier };
 
     const user = await UserModel.findOneAndUpdate(
       query,
       {
         $setOnInsert: {
-          walletAddress: userType === "user" ? identifier : undefined,
           visitorId: userType === "visitor" ? identifier : undefined,
           createdAt: new Date(),
         },
