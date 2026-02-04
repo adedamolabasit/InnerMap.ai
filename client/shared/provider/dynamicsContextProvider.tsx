19ec6b01d160b0f09359c37b293ffca6ea02e3b2
adedamolabasit <adedamolabasit09@gmail.com>
2026-02-04 22:11:18 +0100

    fix: build error

--- client/shared/provider/dynamicsContextProvider.tsx
+++ client/shared/provider/dynamicsContextProvider.tsx
@@ -1,21 +1,22 @@
-"use client";
+// "use client";
 
-import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react-core";
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+// import { DynamicContextProvider} from "@dynamic-labs/sdk-react-core";
+// import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
-export default function WalletProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <DynamicContextProvider
-      settings={{
-        environmentId: "7e55192a-4182-4b62-a439-52bec0b36d0f",
-        walletConnectors: [EthereumWalletConnectors],
-      }}
-    >
-      {children}
-    </DynamicContextProvider>
-  );
-}
+// export default function WalletProvider({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <DynamicContextProvider
+//       settings={{
+//         environmentId: "7e55192a-4182-4b62-a439-52bec0b36d0f",
+//         walletConnectors: [EthereumWalletConnectors],
+        
+//       }}
+//     >
+//       {children}
+//     </DynamicContextProvider>
+//   );
+// }
